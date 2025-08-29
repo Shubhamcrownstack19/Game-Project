dab9e3d8ad8e0e3b326b4d4898eda6e39a5463da
Shubhamcrownstack19 <shubhamjha@crownstack.com>
2025-08-29 09:05:09 -0700

    Add app logic for the first time

---
+++ app.js
@@ -0,0 +1,9 @@
+let userScore = 0;
+let computerScore = 0;
+
+const choices = document.querySelectorAll(".choice");  // Get all choice elements
+
+choices.forEach((choice) =>{
+    console.log(choice);
+choice.addEventListener("click")
+});
