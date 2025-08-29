22fe22301e287fe1efcb3186422e41e4b2a51c5e
Shubhamcrownstack19 <shubhamjha@crownstack.com>
2025-08-29 09:20:50 -0700

    No game logic present in main branch as of now

--- app.js
+++
@@ -1,9 +0,0 @@
-let userScore = 0;
-let computerScore = 0;
-
-const choices = document.querySelectorAll(".choice");  // Get all choice elements
-
-choices.forEach((choice) =>{
-    console.log(choice);
-choice.addEventListener("click")
-});
